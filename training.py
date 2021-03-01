5636e9f872eb1996afd8f84124801d4fdae718ee
mormille <lhmormille@gmail.com>
2021-03-01 21:11:05 +0900

    .

--- training.py
+++ training.py
@@ -15,11 +15,11 @@ import numpy as np
 import torch.nn.functional as F
 from torch import nn
 
-from .models.backbone import Backbone
-from .models.encoder import EncoderModule
-from .models.joiner import Joiner
-from .models.losses import Attention_penalty_factor, Generator_loss
-from .models.unet import UNet
+#from .models.backbone import Backbone
+#from .models.encoder import EncoderModule
+#from .models.joiner import Joiner
+#from models.losses import Attention_penalty_factor, Generator_loss
+#from .models.unet import UNet
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -40,40 +40,43 @@ def data_transform(H,W, normalize:bool=True):
 
 
 
-def train_model(model, inputs, labels, opt_d):
+def train_model(model, generator, inputs, labels, opt_d, model_criterion):
+    
+    
     # Clear discriminator gradients
     opt_d.zero_grad()
-
     
     real_inputs = inputs.to(device) # allow gpu use
     labels = labels.to(device) # allow gpu use
-    # Pass real images through discriminator
-    real_preds, pattn = model(real_inputs)
-    #real_targets = torch.ones(real_images.size(0), 1, device=device)
+    # Pass real images through model
+    real_preds, sattn, pattn = model(real_inputs)
     real_loss = model_criterion(real_preds, labels)
     real_score = torch.mean(real_preds).item()
     
-    _, preds_real = torch.max(outputs, 1)
-    running_real_corrects = torch.sum(preds_real == labels.data)
+    _, preds = torch.max(real_preds, 1) # return the index of the maximum value predicted for that image (used to generate the accuracy)
+     # the sum of the loss of all itens
+    running_corrects = torch.sum(preds == labels.data) # the sum of correct prediction on an epochs
+    #print("TRAIN MODEL FUNCTION - RUNNING CORRECTS",running_corrects)
     
     # Generate fake images
     noised_inputs = generator(real_inputs)
 
     # Pass fake images through discriminator
-    noised_preds, noised_pattn = model(noised_inputs)
+    noised_preds, noised_sattn, noised_pattn = model(noised_inputs)
     noised_loss = model_criterion(noised_preds, labels)
     noised_score = torch.mean(noised_preds).item()
     
-    _, preds_noised = torch.max(outputs, 1)
+    _, preds_noised = torch.max(noised_preds, 1)
     running_noised_corrects = torch.sum(preds_noised == labels.data)
+    #print("TRAIN MODEL FUNCTION - RUNNING NOISED CORRECTS",running_noised_corrects)
 
     # Update discriminator weights
     loss = real_loss + noised_loss
     loss.backward()
     opt_d.step()
-    return loss.item(), real_score, noised_score, running_real_corrects, running_noised_correct
+    return loss.item(), real_score, noised_score, running_corrects, running_noised_corrects, sattn, noised_sattn
 
-def train_generator(inputs, labels, opt_g):
+def train_generator(generator, model, inputs, labels, opt_g, gen_criterion, model_criterion):
     # Clear generator gradients
     opt_g.zero_grad()
     
@@ -84,7 +87,7 @@ def train_generator(inputs, labels, opt_g):
     noised_inputs = generator(real_inputs)
     
     # Try to fool the discriminator
-    preds, pattn = model(noised_inputs)
+    preds, sattn, pattn = model(noised_inputs)
     model_loss = model_criterion(preds, labels)
     loss = gen_criterion(pattn, noised_inputs, real_inputs, model_loss)
     
@@ -94,75 +97,134 @@ def train_generator(inputs, labels, opt_g):
     
     return loss.item()
 
-def fit(model, generator, criterion, optimizer, scheduler, device, num_epochs=25):
+def fit(training_loader, validation_loader, model, generator, model_criterion, gen_criterion, model_optimizer, 
+    gen_optimizer, model_lr_scheduler, generator_lr_scheduler, len_train, len_val, 
+    path='test/TOP_PERFORMANCE_epoch_', epochs=25, start_idx=1):
+    
+    torch.cuda.empty_cache()
+    
+    # Losses & scores
+    losses_g = []
+    losses_d = []
+    real_scores = []
+    noise_scores = []
+    real_corrects = []
+    noised_corrects = []
+    val_real_corrects = []
+    val_noised_corrects = []
+    train_real_attention_maps = []
+    train_adversarial_attention_maps = []
+    val_real_attention_maps = []
+    val_adversarial_attention_maps = []
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    
+    for epoch in range(epochs):
+        start_time = time.time()
+        print('Epoch {}/{}'.format(epoch+1, epochs))
         print('-' * 10)
-        #torch.cuda.empty_cache()
+        
+        running_loss = 0.0
+        running_real_corrects = 0.0
+        running_noised_corrects = 0.0
+        val_running_loss = 0.0
+        val_running_real_corrects = 0.0
+        val_running_noised_corrects = 0.0
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
+        
+        for inputs, labels in training_loader:
+            # Train discriminator
+            loss_d, real_score, noise_score, real_pred, noised_pred, sattn, noised_sattn = train_model(model, generator, inputs, labels, model_optimizer, model_criterion)
+            running_real_corrects +=real_pred
+            running_noised_corrects +=noised_pred
+            #print("FIT FUNCTION - RUNNING REAL CORRECTS",running_real_corrects.item())
+            #print("FIT FUNCTION - RUNNING NOISED CORRECTS",running_noised_corrects.item())
+            # Train generator
+            loss_g = train_generator(generator, model, inputs, labels, gen_optimizer, gen_criterion, model_criterion)
+        
 
-            running_loss = 0.0
-            running_corrects = 0
+        else:
+        #VALIDATION
+            with torch.no_grad(): # to save memory (temporalely set all the requires grad to be false)
+                for val_inputs, val_labels in validation_loader:
+                    val_inputs = val_inputs.to(device) # allow gpu use
+                    val_labels = val_labels.to(device) # allow gpu use
+                    val_noised_input = generator(val_inputs) #passes the image through the network and get the output
+                    val_preds_real, val_real_sattn, val_real_pattn = model(val_inputs)
+                    val_preds_noised, val_noised_sattn, val_noised_pattn = model(val_noised_input)
+                    val_real_loss = model_criterion(val_preds_real, val_labels) #compare output and labels to get the loss 
+                    val_noised_loss = model_criterion(val_preds_noised, val_labels)
+                    
+                    _, val_real_preds = torch.max(val_preds_real, 1) #same as for training
+                    _, val_noised_preds = torch.max(val_preds_noised, 1) #same as for training
+                    
+                    val_loss = val_real_loss + val_noised_loss
+                    val_running_real_corrects += torch.sum(val_real_preds == val_labels.data) #same as for training
+                    val_running_noised_corrects += torch.sum(val_noised_preds == val_labels.data) #same as for training
 
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                torch.cuda.empty_cache()
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+                    val_generator_loss = gen_criterion(val_noised_pattn,val_noised_input,val_inputs,val_noised_loss)
+        
+        
+        model_lr_scheduler.step()
+        generator_lr_scheduler.step()
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+        # Record losses & scores
+        losses_g.append(loss_g)
+        losses_d.append(loss_d)
+        real_scores.append(real_score)
+        noise_scores.append(noise_score)
+        real_corrects.append(running_real_corrects)
+        noised_corrects.append(running_noised_corrects)
+        val_real_corrects.append(val_running_real_corrects)
+        val_noised_corrects.append(val_running_noised_corrects)
+        train_real_attention_maps.append(sattn)
+        train_adversarial_attention_maps.append(noised_sattn)
+        val_real_attention_maps.append(val_real_sattn)
+        val_adversarial_attention_maps.append(val_noised_sattn)
+        
+        # Log losses & scores (last batch)
+        print("Epoch [{}/{}], loss_g: {:.4f}, loss_d: {:.4f}, real_score: {:.4f}, noised_score: {:.4f}".format(
+            epoch+1, epochs, loss_g, loss_d, real_score, noise_score))
+    
+    
+        # Model accuracy       
+        print("EPOCH RUNNING REAL CORRECT PREDICTIONS",running_real_corrects.item())
+        print("EPOCH RUNNING NOISED CORRECTS PREDICTIONS",running_noised_corrects.item())
+        epoch_real_acc = running_real_corrects.item()/ len_train
+        epoch_noised_acc = running_noised_corrects.item()/ len_train
+        print("Epoch [{}/{}], Training - real acc: {:.4f}, noised acc: {:.4f}".format(
+            epoch+1, epochs, epoch_real_acc, epoch_noised_acc))
+        
+        val_epoch_real_acc = val_running_real_corrects.float()/ len_val
+        val_epoch_noised_acc = val_running_noised_corrects.float()/ len_val
+        print("Epoch [{}/{}], Validation - real acc: {:.4f}, noised acc: {:.4f}".format(
+            epoch+1, epochs, val_epoch_real_acc, val_epoch_noised_acc))
+              
+        # Save generated images
+        #save_samples(epoch+start_idx, fixed_latent, show=False)
+        
+        
+        epoch_time_elapsed = time.time() - start_time
+        print('Epoch training complete in {:.0f}m {:.0f}s'.format(
+            epoch_time_elapsed // 60, epoch_time_elapsed % 60))
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs, att, feat = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            if phase == 'train':
-                scheduler.step()
-                
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-            torch.save(model.state_dict(),'test/epoch_' + str(epoch) + '.pth')
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-            #    best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
+        if val_epoch_real_acc > best_acc:
+                best_acc = val_epoch_real_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                print("TOP PERFORMANCE UPDATED")
+                            
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
-
-    # load best model weights
+    
     model.load_state_dict(best_model_wts)
-    torch.save(model.state_dict(),'test/TOP_PERFORMANCE_epoch_' + str(epoch) + '.pth')
-    return model
+    torch.save(model.state_dict(),path + str(epoch) + '.pth')
+    
+    return loss_d, loss_g, real_corrects, noised_corrects, val_real_corrects, val_noised_corrects,train_real_attention_maps, train_adversarial_attention_maps, val_real_attention_maps, val_adversarial_attention_maps
 
